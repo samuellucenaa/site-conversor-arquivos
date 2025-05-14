bd204bd76f6c2e13866ff47bb72dc2bda3897836
samuellucenaa <samuellucenaa1@gmail.com>
2025-05-14 17:36:42 -0300

    finalizando modal com funções de fechar e abrir modal

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+function abrirModal(){
+    document.querySelector('.modal').classList.remove('hidden');
+}
+
+function fecharModal(){
+    document.querySelector('.modal').classList.add('hidden');
+}
+
+function alternarSenha(){
+    let input = document.getElementById('senha');
+    let img = document.getElementById('alternarSenha');
+
+    if(input.type === 'password'){
+        input.type = 'text';
+        img.src = 'images/eye-off-icon.svg';
+        img.alt = 'Esconder senha';
+    } else {
+        input.type = 'password';
+        img.src = 'images/eye-on-icon.svg';
+        img.alt = 'Mostrar senha';
+    }
+}
