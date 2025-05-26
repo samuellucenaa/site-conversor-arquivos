4495374989096f2869751080a35e3b84fc5844ab
samuellucenaa <samuellucenaa1@gmail.com>
2025-05-26 18:10:20 -0300

    criando transição de modal

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,19 @@
 function abrirModal(){
     document.querySelector('.modal-criar').classList.remove('hidden');
+    document.body.classList.add('overflow-hidden');
+    document.querySelector('.modal-entrar').classList.add('hidden');
 }
 
 function abrirModal2(){
     document.querySelector('.modal-entrar').classList.remove('hidden');
     document.querySelector('.modal-criar').classList.add('hidden');
+    document.body.classList.add('overflow-hidden');
 }
 
 function fecharModal(){
     document.querySelector('.modal-criar').classList.add('hidden');
     document.querySelector('.modal-entrar').classList.add('hidden');
-
+    document.body.classList.remove('overflow-hidden');
 }
 
 function trocarImagem(id, novoSrc){
