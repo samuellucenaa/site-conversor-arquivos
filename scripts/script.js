23d8a95084355e1db610c63481a8df0caf1d6191
samuellucenaa <samuellucenaa1@gmail.com>
2025-05-27 18:22:37 -0300

    criando modal recuperar senha

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,26 @@
-function abrirModal(){
+function abrirCriar(){
     document.querySelector('.modal-criar').classList.remove('hidden');
-    document.body.classList.add('overflow-hidden');
     document.querySelector('.modal-entrar').classList.add('hidden');
+    document.body.classList.add('overflow-hidden');
 }
 
-function abrirModal2(){
+function abrirEntrar(){
     document.querySelector('.modal-entrar').classList.remove('hidden');
     document.querySelector('.modal-criar').classList.add('hidden');
+    document.querySelector('.modal-senha').classList.add('hidden')
+    document.body.classList.add('overflow-hidden');
+}
+
+function abrirSenha(){
+    document.querySelector('.modal-senha').classList.remove('hidden');
+    document.querySelector('.modal-entrar').classList.add('hidden')
     document.body.classList.add('overflow-hidden');
 }
 
 function fecharModal(){
     document.querySelector('.modal-criar').classList.add('hidden');
     document.querySelector('.modal-entrar').classList.add('hidden');
+    document.querySelector('.modal-senha').classList.add('hidden');
     document.body.classList.remove('overflow-hidden');
 }
 
@@ -27,10 +35,16 @@ function alternarSenha(){
     if(input.type === 'password'){
         input.type = 'text';
         img.src = 'images/eye-off-icon.svg';
-        img.alt = 'Esconder senha';
     } else {
         input.type = 'password';
         img.src = 'images/eye-on-icon.svg';
-        img.alt = 'Mostrar senha';
     }
+}
+
+function clicou(){
+    const inputEmail = document.querySelector('.input-email');
+    const avisoEmail = document.querySelector('.aviso-email');
+    
+    inputEmail.classList.add('border-red-600');
+    avisoEmail.classList.remove('hidden');
 }
